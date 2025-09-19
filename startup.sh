f3ee4f73bd4cc6fa353aadef502d6271ef443c00
harshalK-SK <harshal.kalve@suprkid.in>
2025-09-19 16:26:50 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Installing system dependencies..."
-apt-get update
-apt-get install -y ffmpeg libsndfile1
-echo "Dependencies installed. Starting application..."
-gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers=2 --worker-class uvicorn.workers.UvicornWorker app:app
